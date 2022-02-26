30c704df5d224933772482df980c5d3487e32750
Kotipallianil <99274818+Kotipallianil@users.noreply.github.com>
2022-02-26 06:45:34 +0530

    assignment2.py

---
+++ assignment2.py
@@ -0,0 +1,12 @@
+n=int(input("enter the number"))
+num=[]
+out[]
+for i in range(n):
+  tem=int(input())
+  num.append
+  num.append(temp)
+for i in num:
+  if i!=0:
+    output.append(i)
+print(output)
+print(len(output))
